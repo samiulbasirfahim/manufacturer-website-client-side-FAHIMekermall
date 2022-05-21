1346377a76ca1090435fab804aba0b7896921b61
samiulbasirfahim360 <samiulbasirfahim360@hotmail.com>
2022-05-22 03:19:29 +0600

    initial setup done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+	const [isDarkTheme, setIsDarkTheme] = useState(localStorage.getItem('isDarkTheme'))
+	const handleDarkTheme = () => {
+		localStorage.setItem('isDarkTheme', !isDarkTheme)
+		setIsDarkTheme(!isDarkTheme)
+	}
+	return (
+		<div className="min-h-screen" data-theme={isDarkTheme ? 'dark' : 'mytheme'}>
+			<button class="btn btn-primary" onClick={handleDarkTheme}>Button</button>
+		</div>
+	)
 }
-
-export default App;
+export default App
