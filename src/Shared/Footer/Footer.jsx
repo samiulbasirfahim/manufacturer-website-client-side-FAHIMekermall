a13b9a47185bb7e0f2e3ab5c437e0a10274f6055
samiulbasirfahim360 <samiulbasirfahim360@hotmail.com>
2022-05-23 14:58:41 +0600

    footer modified

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
 const Footer = () => {
 	return (
@@ -190,29 +191,32 @@ const Footer = () => {
 					</div>
 
 					<div class="sm:flex sm:flex-wrap sm:-mx-4 mt-6 pt-6 sm:mt-12 sm:pt-12 border-t">
-						<div class="sm:w-full px-4 md:w-1/6">
-							<strong>FWR</strong>
+						<div class="sm:w-full px-4 md:w-1/6 flex flex-col justify-start items-start">
+							<strong>Spark</strong>
+							<span>Manufacturer</span>
 						</div>
 						<div class="px-4 sm:w-1/2 md:w-1/4 mt-4 md:mt-0">
 							<h6 class="font-bold mb-2">Address</h6>
 							<address class="not-italic mb-4 text-sm">
 								123 6th St.
 								<br />
-								Melbourne, FL 32904
+								Mymensingh,2200
 							</address>
 						</div>
 						<div class="px-4 sm:w-1/2 md:w-1/4 mt-4 md:mt-0">
-							<h6 class="font-bold mb-2">Free Resources</h6>
+							<h6 class="font-bold mb-2">Tag</h6>
 							<p class="mb-4 text-sm">
-								Use our HTML blocks for <strong>FREE</strong>.
+								Bd best cycle parts <strong>SELLER</strong>.
 								<br />
-								<em>All are MIT License</em>
 							</p>
 						</div>
 						<div class="px-4 md:w-1/4 md:ml-auto mt-6 sm:mt-4 md:mt-0">
-							<button class="px-4 py-2 bg-purple-800 hover:bg-purple-900 rounded text-white">
+							<Link
+								to="/parts"
+								class="px-4 py-2 bg-purple-800 hover:bg-purple-900 rounded text-white"
+							>
 								Explore more
-							</button>
+							</Link>
 						</div>
 					</div>
 				</div>
