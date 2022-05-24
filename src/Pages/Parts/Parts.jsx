ed24af652ab3fd2c52bec7b2ca8e0e9679f442df
samiulbasirfahim360 <samiulbasirfahim360@hotmail.com>
2022-05-24 12:46:21 +0600

    pagination done

--- src/Pages/Parts/Parts.jsx
+++ src/Pages/Parts/Parts.jsx
@@ -9,6 +9,7 @@ const Parts = () => {
 	const [isLoading, setIsLoading] = useState()
 	const [count, setCount] = useState(0)
 	const [totalPage, setTotalPage] = useState(0)
+	const [currentPage, setCurrentPage] = useState(0)
 	useEffect(() => {
 		fetch("http://localhost:4000/part/count")
 			.then((response) => response.json())
@@ -16,16 +17,21 @@ const Parts = () => {
 	}, [])
 	useEffect(() => {
 		setIsLoading(true)
-		fetch(`http://localhost:4000/part?sort=${sort}&limit=${limit}`)
+		fetch(
+			`http://localhost:4000/part?sort=${sort}&limit=${limit}&skip=${
+				currentPage * limit
+			}`
+		)
 			.then((res) => res.json())
 			.then((data) => {
 				setIsLoading(false)
 				setParts(data)
 			})
-	}, [sort, limit])
+	}, [sort, limit, currentPage])
 	useEffect(() => {
 		setTotalPage(Math.ceil(count / limit))
-	}, [limit, count])
+	}, [limit, count, sort])
+	console.log(limit, count)
 	return (
 		<div className="mt-20">
 			{isLoading && <Spinner />}
@@ -68,17 +74,72 @@ const Parts = () => {
 					</select>
 				</div>
 			</div>
-			<div className="container  mx-auto grid justify-items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-12 gap-x-6 pb-24 px-6">
+			<div className="container mx-auto grid justify-items-center grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-12 gap-x-6 pb-24 px-6">
 				{parts.map((part) => (
 					<SinglePart part={part} />
 				))}
 			</div>
-			<select name="" id="">
-				{/* {totalPage((page) => (
-					<option value={page}>{page + 1}</option>
-				))} */}
-			</select>
-			<p>{totalPage}</p>
+			<div className="container mx-auto justify-between flex items-center pb-16">
+				{currentPage !== 0 ? (
+					<button
+						onClick={() => setCurrentPage(currentPage - 1)}
+						class="flex items-center h-8 btn btn-outline"
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="h-5 w-5"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M10 18a8 8 0 100-16 8 8 0 000 16zm.707-10.293a1 1 0 00-1.414-1.414l-3 3a1 1 0 000 1.414l3 3a1 1 0 001.414-1.414L9.414 11H13a1 1 0 100-2H9.414l1.293-1.293z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+						<p>previous</p>
+					</button>
+				) : (
+					<span></span>
+				)}
+				<div className="flex">
+					{[...Array(totalPage).keys()].map((num) => (
+						<button
+							className={
+								currentPage !== num ? "mx-2 btn btn-sm btn-ghost" : "mx-2 btn btn-primary btn-sm"
+							}
+							onClick={() => setCurrentPage(num)}
+						>
+							{num + 1}
+						</button>
+					))}
+				</div>
+
+				{currentPage < totalPage - 1 ? (
+					<button
+						onClick={() => {
+							setCurrentPage(currentPage + 1)
+						}}
+						class="flex items-center h-8 btn btn-outline"
+					>
+						<p>next</p>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="h-5 w-5"
+							viewBox="0 0 20 20"
+							fill="currentColor"
+						>
+							<path
+								fill-rule="evenodd"
+								d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-8.707l-3-3a1 1 0 00-1.414 1.414L10.586 9H7a1 1 0 100 2h3.586l-1.293 1.293a1 1 0 101.414 1.414l3-3a1 1 0 000-1.414z"
+								clip-rule="evenodd"
+							/>
+						</svg>
+					</button>
+				) : (
+					<span></span>
+				)}
+			</div>
 		</div>
 	)
 }
